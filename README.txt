1b1caf458313412e799f3e61a7f472408660dc18
unknown <Tom.Cermelli91@gmail.com>
2019-11-05 12:29:23 +0100

    HTML/CSS Home+Contact

---
+++ README.txt
@@ -0,0 +1,18 @@
+Pré-Requis
+
+Si vous voulez travailler sur ce projet utiliser au moin les versions cité ci-dessous
+-PHP 7.3.10
+-Composer 1.9.0
+-Symfony 4.3.6
+
+
+
+Installation:
+
+•git clone https://github.com/TomCermelli/BraiseDoree
+•composer install
+•php bin/console doctrine:schema:update --force
+
+Bundle utilisé
+
+•SwiftMailer
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class ContactController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/contact", name="contact")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 }
