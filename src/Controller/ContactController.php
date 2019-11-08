e8346fb9dde36a3edbe53ba12b828b05e94c9a3c
unknown <Tom.Cermelli91@gmail.com>
2019-11-08 16:20:35 +0100

    Envoie de mail, pas d'erreur mais teste encore

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,22 +16,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/", name="contact_index", methods={"GET"})
+     * @Route("/", name="contact_index", methods={"GET", "POST"})
      */
-     public function index(Request $request , ContactNotification $notification)
+
+     public function index(Request $request, ContactNotification $notification)
      {
        $contact = new Contact();
        $form = $this->createForm(ContactType::class, $contact); /*On crée un formulaire sous le modèle de ContactType et on lui envoie l'object contact*/
        $form->handleRequest($request);
 
        if($form->isSubmitted() && $form->isValid()) { /*Si le formulaire et soumis et valide, la fonction se lance*/
+      
          $notification->notify($contact);
          $this->addFlash("succes", "Votre email a bien été envoyé"); /*type succes, dans ce cas la on envoie un message à l'utilisateur*/
        }
 
        return $this->render('contact/index.html.twig', [
          'controller_name' => 'ContactController',
-         'form' => $form->createView()
+         'form' => $form->createView(),
        ]);
      }
 }
--- src/Notification/ContactNotification.php
+++ src/Notification/ContactNotification.php
@@ -3,37 +3,28 @@
 namespace App\Notification;
 
 use App\Entity\Contact;
+use Twig\Environment;
 
 class ContactNotification {
 
-  /**
-   *@var \Swift_Mailer
-   */
-   private $mailer;
+public function __construct(\Swift_Mailer $mailer, Environment $renderer){
+  $this->mailer=$mailer;
+  $this->renderer = $renderer;
+}
 
-   /**
-    *@var Environnement
-    */
-    private $renderer;
+public function notify(Contact $contact){
+  $message = (new \Swift_Message ('Agence : ' . $contact->getSubject() ))
+    ->setFrom($contact->getEmail())
+    ->setTo('Tom.Cermelli91@gmail.com')
+    ->setReplyTo($contact->getEmail())
+    ->setBody($this->renderer->render('email/contact.html.twig' , [
+      'contact' =>$contact
+    ]),
+    'text/html'
+  );
+  $this->mailer->send($message);
 
 
-
-  public function __construct(\Swift_Mailer $mailer, Environnement $renderer){
-
-    $this->mailer = $mailer;
-    $this->renderer = $renderer;
-  }
-
-  public function notify(Contact $contact) {
-    $message = (new \Swift_Message('Agence : ' . $contact->getEmail() ) )
-      ->setFrom( 'noreply@server.com')
-      ->setTo('Tom.Cermelli91@gmail.com') /*à quelle adresse on l'envoie*/
-      ->setReplyTo($contact->getEmail()) /*on récupère l'adresse rentré par l'utilisateur*/
-      -setBody($this->renderer->render('emails/contact.html.twig', [
-        'contact'=>contact
-      ]));
-      $this->mailer->send($message);
-
-  }
+}
 
 }
