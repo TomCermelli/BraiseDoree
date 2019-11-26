1cb851c3e47438c45a70c35f0b366c7cf6b5f731
unknown <Tom.Cermelli91@gmail.com>
2019-11-26 16:57:30 +0100

    SEO home,contact,produit,admin

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sonata\SeoBundle\SonataSeoBundle::class => ['all' => true],
 ];
